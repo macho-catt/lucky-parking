eea964ddddfde3f84ccd1c5ce2340bbf19ea8cd8
macho-catt <cacho.matthew@gmail.com>
2021-10-10 19:12:07 -0700

    Fix directory

---
+++ .github/js/wr-pr-trigger/add-linked-issue-labels-to-pr/download-artifact.js
@@ -0,0 +1,41 @@
+/**
+ * Global Variables
+ */
+ var github
+ var context
+ const findLinkedIssue = require('../../utils/find-linked-issue.js')
+ 
+ /**
+  * Parses the Pull Request body for a linked issue, and returns it
+  * @param {Object} g - github object  
+  * @param {Object} c - context object 
+  * @returns - returns the linked issue number
+  */
+ async function main({g, c}) {
+   github = g
+   context = c
+
+  // Retrieve metadata about the artifacts of the last workflow
+  // https://octokit.github.io/rest.js/v18#actions-list-workflow-run-artifacts
+  const artifacts = await github.actions.listWorkflowRunArtifacts({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    run_id: context.payload.workflow_run.id,
+  });
+  console.log('array: ', artifacts.data.artifacts)
+  const artifactData = artifacts.data.artifacts[0]
+  console.log('artifact data: ', artifactData)
+
+  // Download artifact with GET API
+  // https://octokit.github.io/rest.js/v18#actions-download-artifact
+  var download = await github.actions.downloadArtifact({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    artifact_id: artifactData.id,
+    archive_format: 'zip',
+  });
+  const fs = require('fs');
+  fs.writeFileSync(`${github.workspace}/add-linked-issue-labels-to-pr.zip`, Buffer.from(download.data));
+ }
+ 
+ module.exports = main
