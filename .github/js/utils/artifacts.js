e0777fe8cdf1f1c5eba39854b5e0fdeba31b293a
macho-catt <cacho.matthew@gmail.com>
2021-10-11 15:52:56 -0700

    Refactor function

---
+++ .github/js/utils/artifacts.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+async function downloadArtifact(github, context, workspace) {
+  // Retrieve metadata about the artifacts of the last workflow
+  // https://octokit.github.io/rest.js/v18#actions-list-workflow-run-artifacts
+  const artifacts = await github.rest.actions.listWorkflowRunArtifacts({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    run_id: context.payload.workflow_run.id,
+  });
+  console.log('array: ', artifacts.data.artifacts)
+  const artifactData = artifacts.data.artifacts[0]
+  console.log('artifact data: ', artifactData)
+
+  // Download artifact with GET API
+  // https://octokit.github.io/rest.js/v18#actions-download-artifact
+  const download = await github.rest.actions.downloadArtifact({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    artifact_id: artifactData.id,
+    archive_format: 'zip',
+  });
+
+  fs.writeFileSync(`${workspace}/add-linked-issue-labels-to-pr.zip`, Buffer.from(download.data))
+}
+
+export { downloadArtifact }
--- .github/js/wr-pr-trigger/add-linked-issue-labels-to-pr/download-artifact.js
+++ .github/js/wr-pr-trigger/add-linked-issue-labels-to-pr/download-artifact.js
@@ -3,7 +3,7 @@
  */
  var github
  var context
- const fs = require('fs')
+import downloadArtifact from '../../utils/artifacts.js'
  
  /**
   * Parses the Pull Request body for a linked issue, and returns it
@@ -12,31 +12,9 @@
   * @returns - returns the linked issue number
   */
  async function main({g, c}, workspace) {
-   github = g
-   context = c
-
-  console.log('context: ', context)
-  // Retrieve metadata about the artifacts of the last workflow
-  // https://octokit.github.io/rest.js/v18#actions-list-workflow-run-artifacts
-  const artifacts = await github.rest.actions.listWorkflowRunArtifacts({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    run_id: context.payload.workflow_run.id,
-  });
-  console.log('array: ', artifacts.data.artifacts)
-  const artifactData = artifacts.data.artifacts[0]
-  console.log('artifact data: ', artifactData)
-
-  // Download artifact with GET API
-  // https://octokit.github.io/rest.js/v18#actions-download-artifact
-  const download = await github.rest.actions.downloadArtifact({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    artifact_id: artifactData.id,
-    archive_format: 'zip',
-  });
-
-  fs.writeFileSync(`${workspace}/add-linked-issue-labels-to-pr.zip`, Buffer.from(download.data))
+  github = g
+  context = c
+  downloadArtifact(github, context, workspace)
  }
  
- module.exports = main
+module.exports = main
